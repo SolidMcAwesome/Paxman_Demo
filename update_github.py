931b320743a271d2bbe6d26ccb1f830ac61797bc
T <tiseolayinka@gmail.com>
2024-12-03 20:09:53 +0000

    Updated Network Files Upload File

--- update_github.py
+++ update_github.py
@@ -1,4 +1,8 @@
-import os, shutil
+import os, shutil, stat
+from git import Repo
+
+repo = Repo("C:/Users/T/Documents/Work/WWW/TT/Paxman/Paxman_Demo") 
+
 t_file_path = "P:pax_temp.txt"
 tr_file_path = "P:pax_temp_raw.txt"
 tp_file_path = "P:temperature.py"
@@ -9,17 +13,30 @@ t_git_path = git_dir + "pax_temp.txt"
 tr_git_path = git_dir + "pax_temp_raw.txt"
 tp_git_path = git_dir + "temperature.py"
 
+def git_update(): # Draft
+    try:
+        os.system("cd C:/Users/T/Documents/Work/WWW/TT/Paxman/Paxman_Demo")
+        os.system("git add .")
+        os.system("git commit -m 'Updated Network Files'")
+    except:
+        print("Error") 
+    
+    
 def run():
     if os.path.exists(t_git_path):
+        os.chmod(t_git_path, stat.S_IWRITE)
         os.remove(t_git_path)
     shutil.copy2(t_file_path, t_git_path)
     
     if os.path.exists(tr_git_path):
+        os.chmod(tr_git_path, stat.S_IWRITE)
         os.remove(tr_git_path)
     shutil.copy2(tr_file_path, tr_git_path)
     
     if os.path.exists(tp_git_path):
         os.remove(tp_git_path)
     shutil.copy2(tp_file_path, tp_git_path)
+
+    # git_update()
     
 run()
